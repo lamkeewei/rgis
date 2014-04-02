b83c00de6e4cb192e40630612a9699e4513dbb71
Lam Kee Wei <keewei.lam@gmail.com>
2014-04-03 01:49:20 +0800

    Added changes to map directive

--- frontend/app/scripts/directives/map.js
+++ frontend/app/scripts/directives/map.js
@@ -36,44 +36,48 @@ angular.module('rgisApp')
         resizeMap();
 
         console.log(scope.data);
-        L.geoJson(scope.data).addTo(map);
+        var colorbrewer = {
+          GnBu: {
+            3: ['#fde0dd','#fa9fb5','#c51b8a'],
+            4: ['#feebe2','#fbb4b9','#f768a1','#ae017e'],
+            5: ['#feebe2','#fbb4b9','#f768a1','#c51b8a','#7a0177'],
+            6: ['#feebe2','#fcc5c0','#fa9fb5','#f768a1','#c51b8a','#7a0177'],
+            7: ['#feebe2','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177'],
+            8: ['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177'],
+            9: ['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a']
+          }
+        };
+
+        var reduce = _.map(scope.data.features, function(d){
+          return parseFloat(d.properties.level);
+        });
+
+        var color = d3.scale.quantize().
+          domain(d3.extent(reduce)).
+          range(colorbrewer.GnBu[8]);
+
+        var getColor = function(d) {
+          return color(d);
+        };
+        
+        var style = function (feature) {
+            return {
+                fillColor: getColor(feature.properties.Total),
+                weight: 10,
+                opacity: 1,
+                color: getColor(feature.properties.level),
+                fillOpacity: 0.8
+              };
+          };
+
+        L.geoJson(scope.data, {style: style}).addTo(map);
 
         // Add GeoJSON
         // $http.get('Bedok_Population.geojson').then(function(res){
         //   var data = res.data.features;
-        //   var reduce = _.map(data, function(d){
-        //     return d.properties.Total;
-        //   });
 
-        //   var colorbrewer = {
-        //     GnBu: {
-        //       3: ['#fde0dd','#fa9fb5','#c51b8a'],
-        //       4: ['#feebe2','#fbb4b9','#f768a1','#ae017e'],
-        //       5: ['#feebe2','#fbb4b9','#f768a1','#c51b8a','#7a0177'],
-        //       6: ['#feebe2','#fcc5c0','#fa9fb5','#f768a1','#c51b8a','#7a0177'],
-        //       7: ['#feebe2','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177'],
-        //       8: ['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177'],
-        //       9: ['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a']
-        //     }
-        //   };
 
-        //   var color = d3.scale.quantize().
-        //     domain(d3.extent(reduce)).
-        //     range(colorbrewer.GnBu[8]);
 
-        //   var getColor = function(d) {
-        //     return color(d);
-        //   };
-
-        //   var style = function (feature) {
-        //       return {
-        //           fillColor: getColor(feature.properties.Total),
-        //           weight: 2,
-        //           opacity: 1,
-        //           color: 'white',
-        //           fillOpacity: 0.8
-        //         };
-        //     };
 
         //   console.log(res.data);
         //   var geojson = L.geoJson(res.data, {style: style});
